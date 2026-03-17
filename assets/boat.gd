634bf9f234450967b162069412426c33c307cb14
AurianeG <auriane.gonindard@gmail.com>
2026-03-17 09:52:18 -0400

    bateau qui bouge

---
+++ assets/boat.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+
+@export var speed :float = 300.0
+
+
+func _physics_process(delta: float) -> void:
+	
+	var Input_Direction = Vector2(
+		Input.get_action_strength("right")-Input.get_action_strength("left"),
+		Input.get_action_strength("down")-Input.get_action_strength("up")
+	)
+	print(Input_Direction)
+	
+	velocity= Input_Direction * speed;
+
+	move_and_slide()
