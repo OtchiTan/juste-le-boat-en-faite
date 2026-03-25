2497df719739aadfba4b32ad86cb04b712e6916b
Thomas Blanché <thomas.baptiste.blanche@gmail.com>
2026-03-25 02:51:18 +0100

    Rework the BOOOOOATTTT !!!

--- scripts/boat.gd
+++ scripts/boat.gd
@@ -1,17 +1,21 @@
-extends CharacterBody2D
+extends RigidBody2D
 
 class_name Boat
+
+@export var label: Label
+
 var max_speed = 250.0
-var acceleration = 100
 var current_velocity = Vector2.ZERO
 var friction = 0.995
 var lateral_friction = 0.975
-
 var max_rotation_speed = 200
-var min_rotation_speed = 0.05
-var rotation_acceleration = 4
-var current_rotation_speed = 0
 var rotation_friction = 0.95
+var current_rotation_speed = 0
+
+var acceleration = 100
+
+var min_rotation_speed = 0.05
+var rotation_acceleration = 2000
 
 
 var life = 20
@@ -69,60 +73,93 @@ func _physics_process(delta: float) -> void:
 		controller.update(delta)
 	
 	### Tir
-	
 	if want_to_shoot:
 		attack()
 		want_to_shoot = false
 	
 	### Deplacement
+	var forward_dir = global_transform.x # L'avant du bateau (équivalent à Vector2.RIGHT.rotated(rotation))
 	
-	var direction = Vector2.RIGHT.rotated(rotation)
+	if throttle != 0:
+		var force = forward_dir * throttle * acceleration
+		if throttle < 0 and linear_velocity.dot(forward_dir) < 0:
+			force /= 4
+		apply_central_force(force)
 	
-	# Applique les input
-	if(throttle != 0):
-		if(throttle < 0 and current_velocity.normalized().dot(direction.normalized()) < 0):
-			current_velocity += direction * throttle * acceleration * delta / 4
-		else :
-			current_velocity += direction * throttle * acceleration * delta
+	### rotation
+	if steering != 0:
+		var speed_ratio = linear_velocity.length() / 250 
+		var dot_direction = linear_velocity.normalized().dot(forward_dir)
+		var torque = steering * rotation_acceleration * speed_ratio * dot_direction
+		apply_torque(torque)
 	
-	if(steering != 0):
-		current_rotation_speed += steering * delta 														\
-									* (rotation_acceleration * current_velocity.length() / max_speed) 	\
-									* current_velocity.normalized().dot(direction.normalized()) 
+	### Friction
+	var side_dir = global_transform.y
+	var forward_v = side_dir.orthogonal() * linear_velocity.dot(side_dir.orthogonal())
+	var side_v = side_dir * linear_velocity.dot(side_dir)
 	
-	# Limiter les vitesses max
-	if current_velocity.length() > max_speed:
-		current_velocity = current_velocity.normalized() * max_speed
-		
-	if(current_rotation_speed > max_rotation_speed):
-		current_rotation_speed = max_rotation_speed
-		
-	if(current_rotation_speed < -max_rotation_speed):
-		current_rotation_speed = -max_rotation_speed
+	var new_forward_vel = forward_v * friction
+	var new_side_vel = side_v * lateral_friction
+	
+	var transfere_v = (side_v - new_side_vel).length() * 0.4 # suppr le 0.4 et met 2 si t'es un zhomme
+	var transfere_vel = transfere_v * side_dir.orthogonal()
+	
+	linear_velocity = new_forward_vel + new_side_vel + transfere_vel
 	
 	
-	# Applique la rotation
+	### DEBUG
+	label.text = "Debug Print"
 	
-	if(abs(current_rotation_speed) > min_rotation_speed):
-		rotation += current_rotation_speed * delta
 	
-	# Friction
-	var forward_v = current_velocity.dot(direction)
-	var side_v = current_velocity.dot(direction.orthogonal())
 	
-	var new_forward_vel = direction * forward_v * friction
-	var new_side_vel = direction.orthogonal() * side_v * lateral_friction
 	
-	current_velocity = new_forward_vel + new_side_vel
-	
-	current_rotation_speed *= rotation_friction
+	#var direction = Vector2.RIGHT.rotated(rotation)
+	#
+	## Applique les input
+	#if(throttle != 0):
+		#if(throttle < 0 and current_velocity.normalized().dot(direction.normalized()) < 0):
+			#current_velocity += direction * throttle * acceleration * delta / 4
+		#else :
+			#current_velocity += direction * throttle * acceleration * delta
+	#
+	#if(steering != 0):
+		#current_rotation_speed += steering * delta 														\
+									#* (rotation_acceleration * current_velocity.length() / max_speed) 	\
+									#* current_velocity.normalized().dot(direction.normalized()) 
+	#
+	## Limiter les vitesses max
+	#if current_velocity.length() > max_speed:
+		#current_velocity = current_velocity.normalized() * max_speed
+		#
+	#if(current_rotation_speed > max_rotation_speed):
+		#current_rotation_speed = max_rotation_speed
+		#
+	#if(current_rotation_speed < -max_rotation_speed):
+		#current_rotation_speed = -max_rotation_speed
+	#
+	#
+	## Applique la rotation
+	#
+	#if(abs(current_rotation_speed) > min_rotation_speed):
+		#rotation += current_rotation_speed * delta
+	#
+	## Friction
+	#var forward_v = current_velocity.dot(direction)
+	#var side_v = current_velocity.dot(direction.orthogonal())
+	#
+	#var new_forward_vel = direction * forward_v * friction
+	#var new_side_vel = direction.orthogonal() * side_v * lateral_friction
+	#
+	#current_velocity = new_forward_vel + new_side_vel
+	#
+	#current_rotation_speed *= rotation_friction
 	
 	### DEBUG
 	#if (current_velocity.length() != 0):
 		#print_debug(current_velocity.length())
 
-	velocity = current_velocity
-	move_and_slide()
+	#velocity = current_velocity
+	#move_and_slide()
 
 
 func attack() -> void :
