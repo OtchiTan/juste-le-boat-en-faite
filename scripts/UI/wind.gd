3cce2bebffc3eef9c7cfcf9e98ffbf2177dd9300
tibouyou <tibouyou01@gmail.com>
2026-04-01 19:09:55 -0400

    wind effects

---
+++ scripts/UI/wind.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+
+@export var wind_particle: GPUParticles2D
+
+var speed_multiplier : float = 5.0 
+var max_wind_strength : float = 200.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	GameManager.wind_changed.connect(_on_wind_changed)
+	
+	_on_wind_changed(GameManager.wind_direction)
+
+
+func _on_wind_changed(wind_angle: float) -> void:
+	var direction_x = cos(wind_angle)
+	var direction_y = sin(wind_angle)
+	wind_particle.process_material.direction = Vector3(direction_x, direction_y, 0)
+	
+	var current_wind_str = GameManager.wind_str
+	wind_particle.process_material.initial_velocity_min = current_wind_str * speed_multiplier * 0.8
+	wind_particle.process_material.initial_velocity_max = current_wind_str * speed_multiplier * 1.2
+	
+	wind_particle.amount_ratio = clamp(current_wind_str / max_wind_strength, 0.1, 1.0)
